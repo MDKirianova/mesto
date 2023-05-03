60bbf24ef34f22b0998794ca20988801e806a80e
Marina Kiryanova <martinkazz@yandex.ru>
2023-05-03 09:36:45 +0300

    make hotfix after first review

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,28 @@ import { initialCards } from './array.js'
 
 const cardTemplate = document.querySelector('#card-template');
 const gridElementsContainer = document.querySelector('.grid-elements__list');
-let openPopupBtn = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let profileTitle = document.querySelector('.profile__info-title')
-let profileSubtitle = document.querySelector('.profile__info-subtitle')
-let formElement = document.querySelector('.popup__form-element');
-let nameInput = document.querySelector('.popup__input_type_name');
-let jobInput = document.querySelector('.popup__input_type_job');
-let popupAdd = document.querySelector('.popup_add');
-let openAddPopupBtn = document.querySelector('.profile__add-button');
-let addFormElement = document.querySelector('.popup__form-element_add');
-let titleInput = document.querySelector('.popup__input_type_title');
-let linkInput = document.querySelector('.popup__input_type_link');
-const popupZoom = document.querySelector('.zoom-popup');
+const buttonOpenPopupProfile = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const profileTitle = document.querySelector('.profile__info-title')
+const profileSubtitle = document.querySelector('.profile__info-subtitle')
+const formElement = document.querySelector('.popup__form-element');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_job');
+const popupAdd = document.querySelector('.popup-add');
+const buttonOpenPopupAdd = document.querySelector('.profile__add-button');
+const formElementPopupAdd = document.querySelector('.popup__form-element_add');
+const titleInput = document.querySelector('.popup__input_type_title');
+const linkInput = document.querySelector('.popup__input_type_link');
+const popupZoom = document.querySelector('.popup-zoom');
+const popupZoomText = popupZoom.querySelector('.popup-zoom__text');
+const popupZoomImage = popupZoom.querySelector('.popup-zoom__image');
 
 
 function popupOpen(item){
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubtitle.textContent;
   item.classList.add('popup_opened');
 };
 
 function popupClose (item){
-  titleInput.value = '';
-  linkInput.value = '';
   item.classList.remove('popup_opened');
 };
 
@@ -33,16 +31,15 @@ const createCardElement = (name, link) => {
  const cardElement = cardTemplate.content.querySelector('.element').cloneNode(true);
  const elementImage = cardElement.querySelector('.element__image');
  const elementTitle = cardElement.querySelector('.element__title');
- const cardLikeButton = cardElement.querySelector('.element__like');
- const cardDeleteButton = cardElement.querySelector('.element__trash');
- const popupZoomText = popupZoom.querySelector('.zoom-popup__text');
- const popupZoomImage = popupZoom.querySelector('.zoom-popup__image');
+ const buttonLikeCard = cardElement.querySelector('.element__like');
+ const buttonDeleteCard = cardElement.querySelector('.element__trash');
 
  elementTitle.textContent = name;
+ elementImage.alt = name;
  elementImage.src = link;
  
  const handleLike = () => {
-  cardLikeButton.classList.toggle('element__like_active');
+  buttonLikeCard.classList.toggle('element__like_active');
  };
 
  const handleDelete = () => {
@@ -52,11 +49,12 @@ const createCardElement = (name, link) => {
   const handleZoom = () => {
     popupOpen(popupZoom);
     popupZoomImage.src = link;
+    popupZoomImage.alt = name;
     popupZoomText.textContent = name;
-  }
+  };
 
- cardLikeButton.addEventListener('click', handleLike);
- cardDeleteButton.addEventListener('click', handleDelete);
+ buttonLikeCard.addEventListener('click', handleLike);
+ buttonDeleteCard.addEventListener('click', handleDelete);
  elementImage.addEventListener('click', handleZoom);
 
 return cardElement;
@@ -74,17 +72,26 @@ function handleAddFormSubmit (evt) {
   popupClose (popupAdd);
 };
 
-function handleFormSubmit (evt) {
+function handleProfileFormSubmit (evt) {
     evt.preventDefault(); 
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
     popupClose (popup);
 };
 
-openAddPopupBtn.addEventListener('click', () => popupOpen(popupAdd));
-openPopupBtn.addEventListener('click',() =>  popupOpen(popup));
-formElement.addEventListener('submit', handleFormSubmit);
-addFormElement.addEventListener('submit', handleAddFormSubmit);
+buttonOpenPopupAdd.addEventListener('click', () => {
+  titleInput.value = '';
+  linkInput.value = '';
+  popupOpen(popupAdd);
+
+});
+buttonOpenPopupProfile.addEventListener('click',() => {
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+  popupOpen(popup);
+});
+formElement.addEventListener('submit', handleProfileFormSubmit);
+formElementPopupAdd.addEventListener('submit', handleAddFormSubmit);
 
 const allCloseBtns = document.querySelectorAll('.popup__close-btn');
 allCloseBtns.forEach((button) => {
